6c4150e0a3a68462b51c651495a036b48f949152
Ksenia Muhina <1032253531@pfur.ru>
2025-11-29 17:47:14 +0300

    feat(main): add files lab-8

---
+++ labs/lab08/report/task.asm
@@ -0,0 +1,33 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg db 'Функция: f(x) = 15x - 9', 0h
+msgR db 'Результат: ', 0h
+
+SECTION .text
+global _start
+_start:
+ pop ecx
+ pop edx
+ sub ecx, 1
+ mov esi, 0
+
+next:
+ cmp ecx, 0
+ jz _end
+ pop eax
+ call atoi
+ mov ebx, 15
+ mul ebx
+ sub eax, 9
+ add esi, eax
+ loop next
+
+_end:
+ mov eax, msg
+ call sprintLF
+ mov eax, msgR
+ call sprint
+ mov eax, esi
+ call iprintLF
+ call quit
